3f1e52fff27fe711b783ca63878dbd4fe3ede674
Azriel2003 <152828387+Azriel2003@users.noreply.github.com>
2023-12-03 21:27:47 -0600

    Add files via upload

---
+++ js/appDash.js
@@ -0,0 +1,37 @@
+const body = document.querySelector("body"),
+      modeToggle = body.querySelector(".mode-toggle");
+      sidebar = body.querySelector("nav");
+      sidebarToggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if(getMode && getMode ==="dark"){
+    body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if(getStatus && getStatus ==="close"){
+    sidebar.classList.toggle("close");
+}
+
+modeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+        localStorage.setItem("mode", "dark");
+        // Elimina otras clases que afecten el color del texto
+        body.classList.remove("light"); // Si tienes una clase "light"
+    } else {
+        localStorage.setItem("mode", "light");
+        // Elimina otras clases que afecten el color del texto
+        body.classList.remove("dark");
+    }
+});
+
+
+sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    if(sidebar.classList.contains("close")){
+        localStorage.setItem("status", "close");
+    }else{
+        localStorage.setItem("status", "open");
+    }
+})
